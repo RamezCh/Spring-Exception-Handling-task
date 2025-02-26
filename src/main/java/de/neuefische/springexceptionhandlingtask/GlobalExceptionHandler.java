e1a4f289573f353f5442aeff8371d2f079ad31b7
Ramez Chreide <ramiz_chreide@outlook.com>
2025-02-26 10:46:19 +0100

    add: Global NoSuchElementExceptionHandler

--- src/main/java/de/neuefische/springexceptionhandlingtask/GlobalExceptionHandler.java
+++ src/main/java/de/neuefische/springexceptionhandlingtask/GlobalExceptionHandler.java
@@ -6,13 +6,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorMessage handleIllegalArgumentException(IllegalArgumentException exception) {
+        return new ErrorMessage(exception.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ErrorMessage handleNoSuchElementException(NoSuchElementException exception) {
         return new ErrorMessage(exception.getMessage(), LocalDateTime.now());
     }
 }
---
+++ src/test/java/de/neuefische/springexceptionhandlingtask/AnimalIntegrationTest.java
@@ -0,0 +1,16 @@
+package de.neuefische.springexceptionhandlingtask;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalIntegrationTest {
+
+    @Test
+    void getAnimalSpecies() {
+    }
+
+    @Test
+    void getAllAnimals() {
+    }
+}
